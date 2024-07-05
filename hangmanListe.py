351f49e2a62bf7a220cf71c88e4aebe48046ae56
Innoisig <bahatiinnocent36@gmail.com>
2024-07-05 02:47:36 -0700

    jeux Hangman

---
+++ hangmanListe.py
@@ -0,0 +1,2 @@
+word_list = ["aliment", "ananan", "avocat", "agaper", "anonyme", "barnaba", "balama",
+"cynthia", "circonci", "dark", "daniel", "Oignon.","Poireau.","Potiron.","Panais.","Salsifis", "baboon", "camel"]
